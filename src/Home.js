84f11217e5e8a5654661696a520cf8d6683520c3
ccharatt <ccharat.nn@gmail.com>
2023-01-06 00:10:43 +0700

    update form validation

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,39 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
+  const handleImageClick = (e) => {
+    console.log("Image clicked");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
+        <Link onClick={handleImageClick} to="./PokeDex">
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </Link>
         <b>
           Requirement: Try to show the hidden image and make it clickable that
           goes to /pokedex when the input below is "Ready!" remember to hide the
           red text away when "Ready!" is in the textbox.
         </b>
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <input 
+          type="text"
+          name="name"
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+        <span style={{ color: "red" }} hidden={isReady}>I am not ready yet!</span>
       </header>
     </div>
   );
